84797c062e97b18b91285a93b5f157f9803c8466
Толеби Н. Жаксыбай <T.Zhaksybay@jusanmobile.kz>
2024-09-26 15:09:44 +0500

    add new funcstions

--- index.php
+++ index.php
@@ -1,25 +1,54 @@
 <?php
 
-// Настройка параметров
-$myHostIp = '127.0.0.1';
-$server_ip = '192.168.255.24'; // IP сервера
-$port = 514; // Порт по умолчанию для rsyslog (UDP)
-$message = "<134>" . date("M d H:i:s") . " hostname {$myHostIp} This php script was sent by Tolebi\n";
+require __DIR__ . '/vendor/autoload.php';
 
-// Отправка логов через UDP
-$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-if ($sock === false) {
-    die("Не удалось создать сокет: " . socket_strerror(socket_last_error()));
+if (!file_exists(__DIR__ . '/.env')) {
+    die("Файл .env не найден в директории " . __DIR__);
 }
 
-// Отправка сообщения
-$result = socket_sendto($sock, $message, strlen($message), 0, $server_ip, $port);
-if ($result === false) {
-    die("Не удалось отправить сообщение: " . socket_strerror(socket_last_error($sock)));
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$logServerIp = $_ENV['LOG_SERVER_IP'] ?? false;
+$logServerPort = $_ENV['LOG_SERVER_PORT'] ?? false;
+
+if ($logServerIp === false || $logServerPort === false) {
+    die("Ошибка: Не установлены переменные окружения LOG_SERVER_IP или LOG_SERVER_PORT.");
 }
 
-echo "Сообщение успешно отправлено!";
-socket_close($sock);
+/**
+ * @throws Exception
+ */
+function sendLog(string $message, string $serverIp, int $serverPort = 514): void
+{
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    if ($sock === false) {
+        throw new Exception("Не удалось создать сокет: " . socket_strerror(socket_last_error()));
+    }
+
+    if (socket_sendto($sock, $message, strlen($message), 0, $serverIp, $serverPort) === false) {
+        throw new Exception("Не удалось отправить сообщение: " . socket_strerror(socket_last_error($sock)));
+    }
+
+    echo "Сообщение успешно отправлено на сервер {$serverIp}:{$serverPort}\n";
+    socket_close($sock);
+}
+
+try {
+    $hostname = gethostname(); // Получение имени хоста
+    $facility = 1; // LOCAL0
+    $severity = 6; // INFO
+    $priority = ($facility * 8) + $severity; // Формируем приоритет
+
+    // Формирование сообщения
+    $message = "<{$priority}>" . date("M d H:i:s") . " {$hostname} my_php_script: This PHP script was sent by Tolebi\n";
+
+    sendLog($message, $logServerIp, $logServerPort);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
 
 
 
